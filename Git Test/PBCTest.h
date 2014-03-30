7371bb8b345f223ec5c519948e9550c2b62742cb
Piero Castro <Cheroky@Piero-Castros-iMac.local>
2014-03-30 08:03:22 -0700

    Add a test class

---
+++ Git Test/PBCTest.h
@@ -0,0 +1,13 @@
+//
+//  PBCTest.h
+//  Git Test
+//
+//  Created by Piero Castro on 3/30/14.
+//  Copyright (c) 2014 Cheroky Studios. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PBCTest : NSObject
+
+@end
---
+++ Git Test/PBCTest.m
@@ -0,0 +1,13 @@
+//
+//  PBCTest.m
+//  Git Test
+//
+//  Created by Piero Castro on 3/30/14.
+//  Copyright (c) 2014 Cheroky Studios. All rights reserved.
+//
+
+#import "PBCTest.h"
+
+@implementation PBCTest
+
+@end
